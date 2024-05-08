f110f00ab11b68b261fa0270f96e602ae545d745
JatinKadam575 <jatin@appinessworld.com>
2024-05-08 15:11:37 +0530

    added logo

--- my-app/src/components/navigation/index.js
+++ my-app/src/components/navigation/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { ButtonDemo } from "../button";
+import Image from 'next/image'
+import logo from "./logo.svg";
+
 
 const NavigationMenu = () => {
   return (
     <div>
       <div className="h-4 bg-sky-700"></div>
-
       <div className="flex bg-white-900 fixed top-0 w-full justify-between px-16 border-solid pt-6 pb-6	border-1 shadow-sm bg-white border-indigo-600">
-        logo
+    <Image
+      src={logo}
+      width={120}
+      height={120}
+      alt="Picture of the author"
+    />
+  
+
         <div className="flex gap-3 border-solid border-1 border-indigo-600">
           <ButtonDemo buttonStyle="bg-blue-500 text-white hover:bg-blue-600">
             Create a post
