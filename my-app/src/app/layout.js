e39aab08a367b8147c097fe06e4465cba7842408
JatinKadam575 <jatin@appinessworld.com>
2024-05-08 17:02:11 +0530

    fixed hydration error

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -13,9 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <NavigationMenu />
-      <body className={inter.className}>{children}</body>
-      <Footer />
+      <body className={inter.className}>
+        <NavigationMenu />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
